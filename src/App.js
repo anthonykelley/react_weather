22376e59f00dad96417a8ffc05f859f33a60ca7e
Anthony Kelley <anthonymkelley@gmail.com>
2018-02-23 15:54:12 -0700

    add axios for weather api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Table, } from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends Component {
+  state = { weather: [], city: {}, forecast: [] }
+
+  componentDidMount() {
+    axios.get('http://api.openweathermap.org/data/2.5//forecast?zip=84113')
+      .then( res => this.setState({ weather: res.data, city: res.data.city, forecast: res.data.list }))
+      .catch( err => { console.log(err)});
+  }
+
+  displayPeople = () => {
+    return this.state.weather.map( (w, i) => {
+      return(
+        <tr key={i}>
+          <td>{i}</td>
+          <td>{w.city}</td>
+          <td>{w.temperature}</td>
+          <td>{w.wind}</td>
+        </tr>
+      )
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Table striped bordered condensed hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>City</th>
+          <th>Temperature</th>
+          <th>Wind</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </Table>
     );
   }
 }
