a349e882d8318a00c945ee959541169d1091d11f
Anthony Kelley <anthonymkelley@gmail.com>
2018-04-28 01:37:15 -0600

    update weather view // import react semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
 import React, { Component } from 'react';
-import { Table, } from 'react-bootstrap';
+import { Card, Image, Container, } from 'semantic-ui-react';
 import axios from 'axios';
 import { api } from './utils/api'
 
 class App extends Component {
-  state = { weather: [], city: {}, forecast: [] }
+	state = { weather: [] }
 
-  componentDidMount() {
-    axios.get(`http://api.openweathermap.org/data/2.5//forecast?zip=84113&APPID=${api}`)
-      .then( res => this.setState({ weather: res.data, city: res.data.city, forecast: res.data.list }))
-      .catch( err => { console.log(err)});
-  }
+	componentDidMount() {
+		axios.get(`http://api.openweathermap.org/data/2.5/weather?id=5780993&APPID=${api}`)
+			.then( res => this.setState({ weather: res.data }))
+			.catch( err => { console.log(err)});
+	}
 
-  displayPeople = () => {
-    return this.state.weather.map( (w, i) => {
-      return(
-        <tr key={i}>
-          <td>{i}</td>
-          <td>{w.city}</td>
-          <td>{w.temperature}</td>
-          <td>{w.wind}</td>
-        </tr>
-      )
-    })
-  }
+	displayWeather = () => {
+		const { weather } = this.state;
+		if (weather.main) {
+		let temp = (weather.main.temp * (9/5) - 459.67).toFixed(2)
+		let icon = weather.weather[0].icon
+			return(
+				<div>
+				<Image centered src={`http://openweathermap.org/img/w/${icon}.png`} />
+					<Card.Content>
+						<Card.Header textAlign='center'>
+							{weather.name}
+						</Card.Header>
+						<Card.Description textAlign='center'>
+							{temp} &deg;F
+						</Card.Description>
+					</Card.Content>
+					<Card.Content extra textAlign='center'>
+						Humidity: {weather.main.humidity}
+					</Card.Content>
+				</div>
+			)}
+	}
 
-  render() {
-    return (
-      <Table striped bordered condensed hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>City</th>
-          <th>Temperature</th>
-          <th>Wind</th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
-    </Table>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				<Card centered={true}>
+					{this.displayWeather()}
+				</Card>
+			</Container>
+			)
+	}
 }
 
 export default App;
